838b31405cfa4735ad4d1741915f08011f260bdd
WesleyFord <44124160+WesleyFord@users.noreply.github.com>
2020-11-09 16:54:00 -0500

    Update main.py

--- main.py
+++ main.py
@@ -7,38 +7,38 @@ client = discord.Client()
 roles = {}
 nicknames = {}
 kickmessages = ["Hasta la vista, {kickeduser:s}!", "{kickeduser:s} was the weakest link. Goodbye.",
-                "Looks like you're going to the shadow realm, {kickeduser:s}."]
+                "Looks like you're going to the shadow realm, {kickeduser:s}."] #This list can be added to if more vote to kick messages are desired.
 
 @client.event
 async def on_ready():
-    print("Logged in\n")
+    print("Logged in\n") #Confirmation of initialization of the bot.
 
 @client.event
-async def on_member_join(member):
-    if member in roles:
+async def on_member_join(member): 
+    if member in roles: #If the user was previously kicked, this reassigns their previous roles from a dict compiled upon kick.
         for role in roles[member][1:]:
             await member.add_roles(role)
-    if member in nicknames:
+    if member in nicknames: #If the user was previously kicked and had a nickname, this reassigns their previous nickname using a dict compiled upon kick.
         await member.edit(nick=nicknames[member])
 
 @client.event
-async def on_message(message):
+async def on_message(message): #Checks messages to send and determines if it follows the appropriate format of a vote to kick command.
     if message.content.split(" ")[0] == "!vtk":
         await voteToKick(message)
 
 
-def check(reaction, user):
+def check(reaction, user): #Makes sure the reaction type was correct and that the person reacting was neither the person who initiated the kick nor the target of the kick.
     return (user != message.author and user != mention[0] and str(reaction.emoji) == '👞')
 
 async def voteToKick(message):
-    mention = message.mentions
-    if mention[0].id == 729396157859627079:
+    mention = message.mentions #Gathers the target of the vote to kick.
+    if mention[0].id == 729396157859627079: #Prevents users from kicking the bot.
         botmessage = "I'm sorry, {kicker:s}, I'm afraid I can't do that."
         await message.channel.send(botmessage.format(kicker=("<@" + str(message.author.id) + ">")))
-    elif len(mention) != 1:
+    elif len(mention) != 1: #Prevents multiple people from being kicked.
         return
     else:
-        userToKick = mention[0]
+        userToKick = mention[0] 
         votedlist = []
         votes = 0
         botMessage = await message.channel.send("Vote to Kick Initiated: 1/4")
